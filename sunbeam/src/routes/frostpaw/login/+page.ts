5a5e20aff3095115425cd325d4b0be525bb9e10f
bg <bg@gmail.com>
2022-09-27 20:38:10 +0530

    fixes to formatting

--- sunbeam/src/routes/frostpaw/login/+page.ts
+++ sunbeam/src/routes/frostpaw/login/+page.ts
@@ -10,100 +10,100 @@ export const prerender = false;
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ parent, fetch }) {
-    let session = await parent();
-    let url = new URL(session.url);
-    let searchParams = url.searchParams;
+  let session = await parent();
+  let url = new URL(session.url);
+  let searchParams = url.searchParams;
 
-    let retry = "<br/><br/><a href='https://fateslist.xyz'>Try Again?</a>";
+  let retry = "<br/><br/><a href='https://fateslist.xyz'>Try Again?</a>";
 
-    let error = searchParams.get('error');
+  let error = searchParams.get('error');
 
-    if (error) {
-      return {
-        error: error
-      };
-    }
-
-    let code = searchParams.get('code');
-    let state = searchParams.get('state');
-    if (!code || !state) {
-      return {
-        error: 'Invalid code/state' + retry
-      };
-    }
-
-    let stateSplit = state.split('.');
-
-    if (stateSplit.length != 2) {
-      return {
-        error: 'Invalid state'
-      };
-    }
-
-    let nonce = stateSplit[0];
-    let modifier = stateSplit[1];
-
-    let modifierInfo = {};
-
-    info("Login", Base64.decode(modifier))
-
-    try {
-      modifierInfo = JSON.parse(Base64.decode(modifier));
-    } catch (e) {
-      return {
-        error: 'Invalid modifier info'
-      };
-    }
-
-    if (modifierInfo['state'] != nonce) {
-      return {
-        error: 'Invalid nonce'
-      };
-    }
-
-    if (modifierInfo['version'] != 11) {
-      return {
-        error: 'Invalid login request, please try logging in again!!!'
-      };
-    }
-
-    let res = await request(`${api}/oauth2`, {
-      method: 'POST',
-      headers: {
-        'Frostpaw-Server': url.origin,
-      },
-      json: {
-        code: code,
-        state: nonce,
-      },
-      session: session,
-      fetch: fetch,
-      endpointType: "user"
-    });
-
-    let json = {};
-
-    try {
-      json = await res.json();
-    } catch (e) {
-      return {
-        error: `Invalid login request, please try logging in again (${e})`
-      };
-    }
-
-    if (json['state'] == enums.UserState.global_ban) {
-      return {
-        error: `<h1>You are global banned</h1><br/><h2>This is a global ban and as such, you may not login/use our API.</h2><br/>You can try to appeal this ban at <a href="https://fateslist.xyz/staffserver">our ban appeal server</a>`
-      };
-    } else if (!json['token']) {
-      return {
-        error: `Got error: ${JSON.stringify(json)}.`
-      };
-    }
-
+  if (error) {
     return {
-      cookie: Base64.encode(JSON.stringify(json)),
-      href: modifierInfo['href'] || '/',
-      modifier: modifierInfo
+      error: error
     };
   }
+
+  let code = searchParams.get('code');
+  let state = searchParams.get('state');
+  if (!code || !state) {
+    return {
+      error: 'Invalid code/state' + retry
+    };
+  }
+
+  let stateSplit = state.split('.');
+
+  if (stateSplit.length != 2) {
+    return {
+      error: 'Invalid state'
+    };
+  }
+
+  let nonce = stateSplit[0];
+  let modifier = stateSplit[1];
+
+  let modifierInfo = {};
+
+  info("Login", Base64.decode(modifier))
+
+  try {
+    modifierInfo = JSON.parse(Base64.decode(modifier));
+  } catch (e) {
+    return {
+      error: 'Invalid modifier info'
+    };
+  }
+
+  if (modifierInfo['state'] != nonce) {
+    return {
+      error: 'Invalid nonce'
+    };
+  }
+
+  if (modifierInfo['version'] != 11) {
+    return {
+      error: 'Invalid login request, please try logging in again!!!'
+    };
+  }
+
+  let res = await request(`${api}/oauth2`, {
+    method: 'POST',
+    headers: {
+      'Frostpaw-Server': url.origin,
+    },
+    json: {
+      code: code,
+      state: nonce,
+    },
+    session: session,
+    fetch: fetch,
+    endpointType: "user"
+  });
+
+  let json = {};
+
+  try {
+    json = await res.json();
+  } catch (e) {
+    return {
+      error: `Invalid login request, please try logging in again (${e})`
+    };
+  }
+
+  if (json['state'] == enums.UserState.global_ban) {
+    return {
+      error: `<h1>You are global banned</h1><br/><h2>This is a global ban and as such, you may not login/use our API.</h2><br/>You can try to appeal this ban at <a href="https://fateslist.xyz/staffserver">our ban appeal server</a>`
+    };
+  } else if (!json['token']) {
+    return {
+      error: `Got error: ${JSON.stringify(json)}.`
+    };
+  }
+
+  return {
+    cookie: Base64.encode(JSON.stringify(json)),
+    href: modifierInfo['href'] || '/',
+    modifier: modifierInfo
+  };
+}
